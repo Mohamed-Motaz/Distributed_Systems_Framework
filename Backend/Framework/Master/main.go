27f3f98c855f366e7de5803ecc655d8b3638c797
Mohamed Motaz <mohamedmotaz247@yahoo.com>
2022-11-02 14:24:38 +0200

    some more minor cleanup

--- Backend/Framework/Master/main.go
+++ Backend/Framework/Master/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-	"fmt"
+	logger "Framework/Logger"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 //run using -> go run .
 func main() {
 	//run the master
-	master := NewMaster()
-	fmt.Printf("%+v", master)
+	NewMaster()
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	sig := <-signalCh //block until user exits
+	logger.LogInfo(logger.SERVER, logger.ESSENTIAL, "Received a quit sig %+v\nCleaning up resources. Goodbye", sig)
 }
--- Backend/Framework/Master/master.go
+++ Backend/Framework/Master/master.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	mq "Framework/MessageQueue"
+	logger "Framework/Logger"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
 )
 
 func NewMaster() *Master {
 	master := &Master{
-		q: mq.NewMQ(mq.CreateMQAddress(MqUsername, MqPassword, MqHost, MqPort)),
+		//q: mq.NewMQ(mq.CreateMQAddress(MqUsername, MqPassword, MqHost, MqPort)),
 	}
 
 	//start any go routines here
-
+	go master.server()
 	return master
+}
+
+//
+// start a thread that listens for RPCs
+//
+func (master *Master) server() error {
+	rpc.Register(master)
+	rpc.HandleHTTP()
+
+	masterAddr := createMasterAddress()
+	os.Remove(masterAddr)
+	listener, err := net.Listen("tcp", masterAddr)
+
+	if err != nil {
+		logger.FailOnError(logger.MASTER, logger.ESSENTIAL, "Error while listening on socket: %v", err)
+	} else {
+		logger.LogInfo(logger.MASTER, logger.ESSENTIAL, "Listening on socket: %v", masterAddr)
+	}
+
+	go http.Serve(listener, nil)
+	return nil
+}
+
+func createMasterAddress() string {
+	return MyHost + ":" + MyPort
 }
