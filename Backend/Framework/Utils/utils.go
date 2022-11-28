c268f904d3944eac6c3e6058a167fc6f3cb23a63
Mohamed Motaz <mohamedmotaz247@yahoo.com>
2022-11-28 19:20:50 +0200

    add multiple utils

--- Backend/Framework/Utils/utils.go
+++ Backend/Framework/Utils/utils.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"log"
 	"os"
+	"path/filepath"
 )
 
 const IN_DOCKER bool = false
@@ -13,3 +15,30 @@ func GetEnv(key, fallback string) string {
 	return fallback
 }
 
+func RemoveFilesWithStartName(name string) {
+	files, err := filepath.Glob(name + "*")
+	if err != nil {
+		log.Printf("Error while getting files that start with name %+v \n", name)
+		return
+	}
+	for _, f := range files {
+		if err := os.Remove(f); err != nil {
+			log.Printf("Error while removing file %+v \n", f)
+
+		}
+	}
+}
+
+func CreateAndWriteToFile(name string, data []byte) error {
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		return err
+	}
+
+	return f.Close()
+}
