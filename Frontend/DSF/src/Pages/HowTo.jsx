e0e9f79f7fac7ac9051ba358952e767e0f7f74b2
KareemE125 <kareemezzat.125@gmail.com>
2023-02-15 22:30:23 +0200

    How to use page

--- Frontend/DSF/src/Pages/HowTo.jsx
+++ Frontend/DSF/src/Pages/HowTo.jsx
@@ -1,7 +1,43 @@
+import { Accordion } from 'flowbite-react'
 import React from 'react'
 
 export default function HowTo() {
-  return (
-    <div>HowTo</div>
-  )
+
+  const StepsList = [
+    {
+      title: "Step 1",
+      content: "Lorem Ipsum is simply dummy text."
+    },
+    {
+      title: "Step 2",
+      content: "Lorem Ipsum is simply dummy text."
+    },
+    {
+      title: "Step 3",
+      content: "Lorem Ipsum is simply dummy text."
+    },
+    {
+      title: "Step 4",
+      content: "Lorem Ipsum is simply dummy text."
+    },
+    {
+      title: "Step 5",
+      content: "Lorem Ipsum is simply dummy text."
+    },
+  ]
+
+  return <Accordion collapseAll={true}>
+    {
+      StepsList.map(item => <Accordion.Panel key={item.title}>
+        <Accordion.Title>
+          {item.title}
+        </Accordion.Title>
+        <Accordion.Content>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            {item.content}
+          </p>
+        </Accordion.Content>
+      </Accordion.Panel>)
+    }
+  </Accordion>
 }
